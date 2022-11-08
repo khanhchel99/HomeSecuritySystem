b8002602da76162c902349206af9f59eef715801
Ian <iguenthe@uwo.ca>
2022-11-07 20:31:30 -0500

    sensor

--- sensor.h
+++ sensor.h
@@ -1,15 +1,26 @@
 #ifndef SENSOR_H
 #define SENSOR_H
-
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 class Sensor
 {
 private:
+    int sensorPin;
     //PhysicalSensor physicalSensor;
     bool disturbance;
+    
+    static Sensor* _instance;
+    
+    //prevent copying and assignment
+    Sensor(const Sensor& other) = delete;
+    Sensor& operator=(const Sensor& other) = delete;
+    Sensor();
 
 public:
-    Sensor();
+    
+    static Sensor *instance();
     void setDisturbance(bool distChange);
     bool getDisturbance();
 };
